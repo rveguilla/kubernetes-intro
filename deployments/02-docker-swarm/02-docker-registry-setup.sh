9579fd7b4797e602d77cb14e6af783bbc3132f9c
Ricardo Veguilla <veguilla@gmail.com>
2018-01-28 00:31:04 -0500

    Fix typos.

--- deployments/02-docker-swarm/02-docker-registry-setup.sh
+++ deployments/02-docker-swarm/02-docker-registry-setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo On the browser:
-echo    1. Create a Docker Hun account at: https://hub.docker.com
+echo    1. Create a Docker Hub account at: https://hub.docker.com
 echo    2. Create a new repo named: 'example-app'
 echo
 echo On the terminal, run:
