bb7190303e8eb62351967b0fd603c755d7774ae6
Ricardo Veguilla <veguilla@gmail.com>
2018-08-07 00:23:07 -0400

    Updated kubernetes-gcp-cli provision/unprovision steps

--- deployments/04-kubernetes-gcp-cli/01-provision.sh
+++ deployments/04-kubernetes-gcp-cli/01-provision.sh
@@ -1,22 +1,32 @@
 #!/usr/bin/env bash
 
-BILLING_ACCOUNT=$(gcloud alpha billing accounts list --filter="open = true" --format "value(name)")
-
-if [ -z "${BILLING_ACCOUNT}" ]; then
-    echo "Error: could not find a billing account. Please got to https://console.cloud.google.com/billing and configure a billing account"
+if [ -z "${GCLOUD_PROJECT}" ]; then
+    echo "Error: GCLOUD_PROJECT environment variable required."
     exit -1
+else
+    BILLING_ACCOUNTS=$(gcloud alpha billing accounts list --filter="open = true" --format "value(name)")
+    BILLING_ENABLED="False"
+    for ACCOUNT in $BILLING_ACCOUNTS; do
+        BILLING_ENABLED=$(gcloud alpha billing projects list --billing-account ${ACCOUNT} --filter "projectId=${GCLOUD_PROJECT}" --format="value(billingEnabled)")
+        if [ "${BILLING_ENABLED}" == "True" ]; then        
+            break
+        fi    
+    done  
+  
+    if [ "${BILLING_ENABLED}" == "False" ]; then
+        echo "Error: GCLOUD_PROJECT is not currently linked with an active billing account."
+        exit -1 
+    fi  
 fi
+
 
 KUBE_CLUSTER_NAME=${KUBE_CLUSTER_NAME:-exampleapp-kube-cluster}
 REGION=${REGION:-us-central1}
 ZONE=$REGION-a
 NODE_COUNT=${NODE_COUNT:-1}
 
-UUID=$(od -x /dev/urandom | head -1 | awk '{OFS="-"; print $2$3$4}')
-gcloud projects create example-app-${UUID} --set-as-default
-gcloud alpha billing projects link $(gcloud config get-value project) --billing-account=${BILLING_ACCOUNT}
-
+gcloud config set project ${GCLOUD_PROJECT}
 gcloud config set compute/region ${REGION}
 gcloud config set compute/zone ${ZONE}
-gcloud services enable container.googleapis.com
+gcloud services enable container.googleapis.com compute.googleapis.com
 gcloud container clusters create ${KUBE_CLUSTER_NAME} --max-nodes=${NODE_COUNT}
--- deployments/04-kubernetes-gcp-cli/09-unprovision.sh
+++ deployments/04-kubernetes-gcp-cli/09-unprovision.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-gcloud projects delete $(gcloud config get-value project)  --quiet
+KUBE_CLUSTER_NAME=${KUBE_CLUSTER_NAME:-exampleapp-kube-cluster}
+gcloud container clusters delete ${KUBE_CLUSTER_NAME} --quiet
 
 CONTAINER_REGISTRY=gcr.io/$(gcloud config get-value project)
 for IMAGE in $(gcloud container images list --repository ${CONTAINER_REGISTRY} --format="value(name)"); do 
