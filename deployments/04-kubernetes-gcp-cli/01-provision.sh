84e541f164d48071b7f3491d1d8720ac10313807
Ricardo Veguilla <veguilla@gmail.com>
2018-08-12 18:51:31 -0400

    on kubernetes-gcp-cli example, set num_nodes=3

--- deployments/04-kubernetes-gcp-cli/01-provision.sh
+++ deployments/04-kubernetes-gcp-cli/01-provision.sh
@@ -23,10 +23,10 @@ fi
 KUBE_CLUSTER_NAME=${KUBE_CLUSTER_NAME:-exampleapp-kube-cluster}
 REGION=${REGION:-us-central1}
 ZONE=$REGION-a
-NODE_COUNT=${NODE_COUNT:-1}
+NODE_COUNT=${NODE_COUNT:-3}
 
 gcloud config set project ${GCLOUD_PROJECT}
 gcloud config set compute/region ${REGION}
 gcloud config set compute/zone ${ZONE}
 gcloud services enable container.googleapis.com compute.googleapis.com
-gcloud container clusters create ${KUBE_CLUSTER_NAME} --max-nodes=${NODE_COUNT}
+gcloud container clusters create ${KUBE_CLUSTER_NAME} --num-nodes=${NODE_COUNT}
