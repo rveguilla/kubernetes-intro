574eafe1641aff2cb498fc5adcbfcb2020dcae9a
Ricardo Veguilla <veguilla@gmail.com>
2019-10-30 23:04:18 -0500

    Run minikube using cri-o container runtime

--- deployments/local/minikube/01-provision.sh
+++ deployments/local/minikube/01-provision.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 MINIKUBE_DRIVER=${MINIKUBE_DRIVER:-virtualbox}
-minikube start --cpus 4 --memory 4096 --vm-driver ${MINIKUBE_DRIVER}
+MINIKUBE_CONTAINER_RUNTIME=${MINIKUBE_CONTAINER_RUNTIME:-cri-o}
+minikube start \
+   --vm-driver ${MINIKUBE_DRIVER} \
+   --container-runtime=${MINIKUBE_CONTAINER_RUNTIME} \
+   --cpus 4 --memory 4096 
 
 echo "Adding minikube.host to /etc/hosts ..."
 echo "$(minikube ip) example-app-nodejs.minikube.local example-app-java.minikube.local" | sudo tee -a /etc/hosts
